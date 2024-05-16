e8695717b64e8526722ceab2c9c8615fb0e9fe91
andreea.lupascu <andreea.lupascu@raro.eu>
2024-05-16 15:40:39 +0300

    chnages

--- auth/app.py
+++ auth/app.py
@@ -3,24 +3,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 
 raw_password = 'password'
-HASHED_PASSWORD = generate_password_hash(raw_password, method='sha256')
+HASHED_PASSWORD = generate_password_hash(raw_password, method='pbkdf2:sha256')
 
 app = Flask(__name__)
 
 authenticated = False
 
-@app.route('/auth-app/', methods=['GET', 'POST'])
 
+@app.route('/auth-app/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        username = request.form['username'];
-        password = request.form['password'];
+        username = request.form['username']
+        password = request.form['password']
         if username == 'Mircea' and check_password_hash(HASHED_PASSWORD, password):
             response = requests.get('http://ce ma fac? :(')
             return response.text
         else:
             return "Login failed"
     return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081, debug=True)
